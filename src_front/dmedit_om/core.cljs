2bd87f7c201cc962343106a9dfedacabcfe6fb42
David Mohl <me@dave.cx>
2015-10-14 14:49:14 +0900

    stuffed codemirror into a react component. Mounting through om

--- src_front/dmedit_om/core.cljs
+++ src_front/dmedit_om/core.cljs
@@ -9,14 +9,37 @@
  :websocket-url   "ws://localhost:3449/figwheel-ws"
  :jsload-callback 'mount-root)
 
-(defonce app-state (atom {:message "Hello om world!"}))
+(defonce app-state (atom {}))
+
+(defn resize-codemirror [w h]
+  (let [codemirror (aget
+                    (. js/document
+                       (getElementsByClassName "CodeMirror")) 0)]
+    (.setAttribute codemirror "style" (str "width:"w"px;height:"h"px;"))))
+
+(defn codemirror [state owner]
+  (reify
+      om/IWillUpdate
+    (will-update [this next-props next-state]
+      (println next-props)
+      (println next-state)
+      (resize-codemirror (get-in state [:window :w]) (get-in state [:window :h])))
+      om/IRenderState
+    (render-state [this state]
+      (dom/div nil))
+    om/IDidMount
+    (did-mount [_]
+      (swap! app-state assoc :codemirror
+             (js/CodeMirror (om/get-node owner)
+              #js {:matchBrackets true :autoCloseBrackets true :lineWrapping true}))
+      (resize-codemirror (get-in state [:window :w]) (get-in state [:window :h])))))
 
 (defn mount-root []
   (om/root
    (fn [state owner]
      (reify om/IRender
        (render [_]
-         (dom/h1 nil (:message state)))))
+         (om/build codemirror state))))
    app-state
    {:target (. js/document
                (getElementById "app"))}))
@@ -25,11 +48,8 @@
   [event]
   (let [w (.-innerWidth js/window)
         h (.-innerHeight js/window)]
-
-    (let [codemirror (aget
-                      (. js/document
-                         (getElementsByClassName "CodeMirror")) 0)]
-      (.setAttribute codemirror "style" (str "width:"w"px;height:"h"px;")))))
+    ;(om/set-state! app-state :window (hash-map :w w :h h))
+    (swap! app-state assoc :window (hash-map :w w :h h))))
 
 (defn get-text []
   (.getValue (@app-state :codemirror)))
@@ -40,20 +60,10 @@
 (defn textchange-handler [event]
   (.log js/console (parse-markdown (get-text))))
 
-(defonce codemirror-init
-  (->>
-   (. js/CodeMirror fromTextArea
-      (. js/document (getElementById "code"))
-      (js-obj "lineWrapping" true))
-   (swap! app-state assoc :codemirror)))
-
 (defonce resize-init
   (.addEventListener js/window "resize" windowresize-handler))
 
-(defonce change-init
-  (.on (@app-state :codemirror) "change" textchange-handler))
-
-
 (windowresize-handler nil)
+(mount-root)
 
 
