4fa4541b080e07ef82f8f44aaad4a046bb4c564a
David Mohl <me@dave.cx>
2015-10-20 17:01:39 +0900

    style changes - now it actually looks kinda good :)

--- src_front/dmedit_om/components/codemirror.cljs
+++ src_front/dmedit_om/components/codemirror.cljs
@@ -17,6 +17,15 @@
   Object
   (render [this]
           (dom/div #js {:id "codemirror-target"}))
+  (componentWillReceiveProps [this next-props]
+                             (let [{:keys [app/force-overwrite app/text]} next-props]
+                               ;; Ignore overwriting if force-overwrite is not true
+                               ;; This is because the cursor would jump if we overwrite
+                               ;; the entire thing with every keypress. Not good, no no
+                               (if force-overwrite
+                                 (do
+                                   (.setValue (.getDoc (@local-state :codemirror)) text)
+                                   ((@local-state :overwrite-callback))))))
   (componentDidMount [this]
                      (let [codemirror 
                            (js/CodeMirror (gdom/getElement "codemirror-target")
@@ -27,7 +36,8 @@
                                                })]
                        (swap! local-state assoc :codemirror codemirror)
 
-                       (let [{:keys [app/text text-callback]} (om/props this)]
+                       (let [{:keys [app/text text-callback overwrite-callback]} (om/props this)]
+                         (swap! local-state assoc :overwrite-callback overwrite-callback)
                          (.setValue (.getDoc codemirror) text)
                          (.on codemirror "change"
                               #(text-callback (.getValue codemirror)))))
--- src_front/dmedit_om/components/markdown.cljs
+++ src_front/dmedit_om/components/markdown.cljs
@@ -4,12 +4,23 @@
             [goog.dom :as gdom]
             [figwheel.client :as fw :include-macros true])) 
 
+
+(defn fill-markdown [_]
+  (let [md (. js/document (getElementById "parsed-markdown"))
+        h (.-innerHeight js/window)]
+    (.setAttribute md "style" (str "height:"h"px;"))))
+
 (defui MarkdownComponent
   Object
   (render [this]
           (let [{:keys [app/html]} (om/props this)]
             (dom/div #js {:id "parsed-markdown"}
                      (dom/div #js {:className "markdown-body"
-                                   :dangerouslySetInnerHTML #js {:__html html}})))))
+                                   :dangerouslySetInnerHTML #js {:__html html}}))))
+
+  (componentWillMount [this]
+                        (.addEventListener js/window "resize" fill-markdown))
+  (componentWillUnmount [this]
+                        (.removeEventListener js/window "resize" fill-markdown)))
 
 (def markdown (om/factory MarkdownComponent))
