669f0a740e94657b8f6bc9aa302a85860708ad07
David Mohl <me@dave.cx>
2015-10-24 17:13:10 +0900

    om.next version bump

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject markright "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/dvcrn/markright/"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145" :exclusions [org.apache.ant/ant]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.omcljs/om "1.0.0-alpha3"]
+                 [org.omcljs/om "1.0.0-alpha7"]
                  [com.cemerick/piggieback "0.2.1"]
                  [org.clojure/tools.nrepl "0.2.10"]
                  [figwheel "0.4.0"]]
