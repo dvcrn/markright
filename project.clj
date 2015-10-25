99cf597012662cb1700208820a19238226ece2e9
David Mohl <me@dave.cx>
2015-10-25 18:01:44 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145" :exclusions [org.apache.ant/ant]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.omcljs/om "1.0.0-alpha7"]
+                 [org.omcljs/om "1.0.0-alpha8"]
                  [com.cemerick/piggieback "0.2.1"]
                  [org.clojure/tools.nrepl "0.2.10"]
                  [figwheel "0.4.0"]]
