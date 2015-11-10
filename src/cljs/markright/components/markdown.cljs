d78fd3dff0895dfe31f2e97b8343627f44c6c502
David Mohl <me@dave.cx>
2015-11-10 16:12:54 +0900

    added .openExternal call for all rendered <a> tags, closes #20

--- src/cljs/markright/components/markdown.cljs
+++ src/cljs/markright/components/markdown.cljs
@@ -8,17 +8,29 @@
         h (.-innerHeight js/window)]
     (.setAttribute md "style" (str "height:" h "px;"))))
 
+(defn generate-open-external-string [url]
+  (str "require('shell').openExternal('" (js/decodeURIComponent url) "')"))
+
+(defn parse-urls! []
+  (let [a-tags (.getElementsByTagName js/document "a")]
+     (doseq [tag (array-seq a-tags)]
+       (.setAttribute tag "onclick" (generate-open-external-string (.-href tag)))
+       (.setAttribute tag "href" "#"))))
+
 (defui MarkdownComponent
   Object
   (render [this]
-    (let [{:keys [app/html]} (om/props this)]
-      (dom/div #js {:id "parsed-markdown"}
-        (dom/div #js {:className "markdown-body"
-                      :dangerouslySetInnerHTML #js {:__html html}}))))
-
+          (let [{:keys [app/html]} (om/props this)]
+            (dom/div #js {:id "parsed-markdown"}
+                     (dom/div #js {:className "markdown-body"
+                                   :dangerouslySetInnerHTML #js {:__html html}}))))
   (componentWillMount [this]
-    (.addEventListener js/window "resize" fill-markdown))
+                      (.addEventListener js/window "resize" fill-markdown))
   (componentWillUnmount [this]
-    (.removeEventListener js/window "resize" fill-markdown)))
+                        (.removeEventListener js/window "resize" fill-markdown))
+  (componentDidMount [this]
+                     (parse-urls!))
+  (componentDidUpdate [this prev-props prev-state]
+                     (parse-urls!)))
 
 (def markdown (om/factory MarkdownComponent))
