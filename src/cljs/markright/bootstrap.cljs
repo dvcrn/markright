1ae5635deda3c28c80ab78d37ac63aa37beed4df
David Mohl <git@d.sh>
2025-12-05 11:32:14 +0700

    Limit highlight.js to core languages for CI

--- src/cljs/markright/bootstrap.cljs
+++ src/cljs/markright/bootstrap.cljs
@@ -1,10 +1,24 @@
 (ns markright.bootstrap
   (:require ["react" :as react]
             ["react-dom" :as react-dom]
-            ["highlight.js" :as hljs]))
+            ["highlight.js/lib/core" :as hljs]
+            ["highlight.js/lib/languages/javascript" :as lang-js]
+            ["highlight.js/lib/languages/xml" :as lang-xml]
+            ["highlight.js/lib/languages/markdown" :as lang-md]
+            ["highlight.js/lib/languages/css" :as lang-css]
+            ["highlight.js/lib/languages/json" :as lang-json]
+            ["highlight.js/lib/languages/bash" :as lang-bash]))
 
 (set! js/React react)
 (set! js/ReactDOM react-dom)
+
+(.registerLanguage hljs "javascript" lang-js)
+(.registerLanguage hljs "xml" lang-xml)
+(.registerLanguage hljs "markdown" lang-md)
+(.registerLanguage hljs "css" lang-css)
+(.registerLanguage hljs "json" lang-json)
+(.registerLanguage hljs "bash" lang-bash)
+
 (set! js/hljs hljs)
 
 (def marked (js/require "marked"))
